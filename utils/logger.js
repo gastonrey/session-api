d1656f388413337a2f1d54cc98327464e56b4e04
Gaston Rey <gaston.rey@haufe.com>
2023-01-11 16:45:09 +0100

    log errors to console

--- utils/logger.js
+++ utils/logger.js
@@ -3,6 +3,6 @@ const path = require('path')
 
 module.exports = {
     log(message, level = 'info', callback) {
-        return `${level} ${new Date().toISOString()} - ${message instanceof Error ? message.stack : message}\n`
+        console.log(`${level} ${new Date().toISOString()} - ${message instanceof Error ? message.stack : message}\n`)
     }
 }
